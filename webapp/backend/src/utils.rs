72cc189921228c606369320e1f0bb549e4be55ea
Your Name <you@example.com>
2024-07-27 18:56:20 +0900

    fix pass

--- webapp/backend/src/utils.rs
+++ webapp/backend/src/utils.rs
@@ -1,8 +1,10 @@
-use argon2::{
-    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
-    Argon2,
-};
+// use argon2::{
+//     password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+//     Argon2,
+// };
 use rand::Rng;
+
+use bcrypt::{hash, verify, DEFAULT_COST};
 
 use crate::errors::AppError;
 
@@ -18,28 +20,43 @@ pub fn generate_session_token() -> String {
     token
 }
 
+// pub fn hash_password(password: &str) -> Result<String, AppError> {
+//     let password_bytes = password.as_bytes();
+//     let salt = SaltString::generate(&mut OsRng);
+
+//     // Argon2 with default params (Argon2id v19)
+//     let argon2 = Argon2::default();
+
+//     // Hash password to PHC string ($argon2id$v=19$...)
+//     match argon2.hash_password(password_bytes, &salt) {
+//         Ok(hashed_password_bytes) => Ok(hashed_password_bytes.to_string()),
+//         Err(_) => Err(AppError::InternalServerError),
+//     }
+// }
+
+// pub fn verify_password(hashed_password: &str, input_password: &str) -> Result<bool, AppError> {
+//     let input_password_bytes = input_password.as_bytes();
+//     let parsed_hash = match PasswordHash::new(hashed_password) {
+//         Ok(hash) => hash,
+//         Err(_) => return Err(AppError::InternalServerError),
+//     };
+//     match Argon2::default().verify_password(input_password_bytes, &parsed_hash) {
+//         Ok(_) => Ok(true),
+//         Err(_) => Ok(false),
+//     }
+// }
+
 pub fn hash_password(password: &str) -> Result<String, AppError> {
-    let password_bytes = password.as_bytes();
-    let salt = SaltString::generate(&mut OsRng);
-
-    // Argon2 with default params (Argon2id v19)
-    let argon2 = Argon2::default();
-
-    // Hash password to PHC string ($argon2id$v=19$...)
-    match argon2.hash_password(password_bytes, &salt) {
-        Ok(hashed_password_bytes) => Ok(hashed_password_bytes.to_string()),
+    // ハッシュ化する際のコストをデフォルトに設定（デフォルトは12）
+    match hash(password, DEFAULT_COST) {
+        Ok(hashed_password) => Ok(hashed_password),
         Err(_) => Err(AppError::InternalServerError),
     }
 }
 
 pub fn verify_password(hashed_password: &str, input_password: &str) -> Result<bool, AppError> {
-    let input_password_bytes = input_password.as_bytes();
-    let parsed_hash = match PasswordHash::new(hashed_password) {
-        Ok(hash) => hash,
-        Err(_) => return Err(AppError::InternalServerError),
-    };
-    match Argon2::default().verify_password(input_password_bytes, &parsed_hash) {
-        Ok(_) => Ok(true),
-        Err(_) => Ok(false),
+    match verify(input_password, hashed_password) {
+        Ok(is_valid) => Ok(is_valid),
+        Err(_) => Err(AppError::InternalServerError),
     }
 }
